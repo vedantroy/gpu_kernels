7d2f1a181872926ff2915191c538fd9968830cc9
Your Name <vroy101@gmail.com>
2024-01-06 09:56:24 -0800

    fix indent

--- allreduce/csrc/my_sync/sync.cuh
+++ allreduce/csrc/my_sync/sync.cuh
@@ -49,50 +49,50 @@ struct RankSignals {
   volatile Signal *signals[8];
 };
 
-  class Sync {
-      public:
-        int rank_;
-        int world_size_;
+class Sync {
+    public:
+      int rank_;
+      int world_size_;
 
-        // below are device pointers
-        RankSignals sg_;
-        BarrierState *barrier_state_;
+      // below are device pointers
+      RankSignals sg_;
+      BarrierState *barrier_state_;
 
-        std::vector<void *> ipc_handles_;
+      std::vector<void *> ipc_handles_;
 
-        Sync(BarrierState *barrier_state, const cudaIpcMemHandle_t *handles,
-             const std::vector<int64_t> &offsets, int rank)
-             : rank_(rank),
-               world_size_(offsets.size()),
-               barrier_state_(barrier_state) {
-          for (int i = 0; i < world_size_; i++) {
-            BarrierState *rank_barrier_state;
-            if (i != rank_) {
-              char *handle;
-              CUDACHECK(cudaIpcOpenMemHandle((void **)&handle, handles[i],
-                                             cudaIpcMemLazyEnablePeerAccess));
-              ipc_handles_.push_back(handle);
-              handle += offsets[i];
-              rank_barrier_state = (BarrierState *)handle;
-            } else {
-              rank_barrier_state = barrier_state_;
-            }
-            // This is pure pointer math (no access to on-device memory)
-            sg_.signals[i] = &rank_barrier_state->sg;
+      Sync(BarrierState *barrier_state, const cudaIpcMemHandle_t *handles,
+           const std::vector<int64_t> &offsets, int rank)
+           : rank_(rank),
+             world_size_(offsets.size()),
+             barrier_state_(barrier_state) {
+        for (int i = 0; i < world_size_; i++) {
+          BarrierState *rank_barrier_state;
+          if (i != rank_) {
+            char *handle;
+            CUDACHECK(cudaIpcOpenMemHandle((void **)&handle, handles[i],
+                                           cudaIpcMemLazyEnablePeerAccess));
+            ipc_handles_.push_back(handle);
+            handle += offsets[i];
+            rank_barrier_state = (BarrierState *)handle;
+          } else {
+            rank_barrier_state = barrier_state_;
           }
+          // This is pure pointer math (no access to on-device memory)
+          sg_.signals[i] = &rank_barrier_state->sg;
         }
+      }
 
-        void sync_test(int blocks, int threads) {
-          if (threads % 32 != 0 || threads <= 32) {
-            throw std::runtime_error("Threads must be a multiple of 32 greater than 32");
+      void sync_test(int blocks, int threads) {
+        if (threads % 32 != 0 || threads <= 32) {
+          throw std::runtime_error("Threads must be a multiple of 32 greater than 32");
+        }
+        sync_test_kernel<<<blocks, threads>>>();
+      }
+
+      ~Sync() {
+          for (auto ptr : ipc_handles_) {
+            CUDACHECK(cudaIpcCloseMemHandle(ptr));
           }
-          sync_test_kernel<<<blocks, threads>>>();
-        }
-
-        ~Sync() {
-            for (auto ptr : ipc_handles_) {
-              CUDACHECK(cudaIpcCloseMemHandle(ptr));
-            }
-        }
-  };
+      }
+};
 }
