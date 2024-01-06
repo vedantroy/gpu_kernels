8a96abdf215cef3cda3c044af03308d473a7f7e8
Your Name <vroy101@gmail.com>
2024-01-05 18:44:33 -0800

    try to compile sync test

--- allreduce/csrc/my_sync/sync.cuh
+++ allreduce/csrc/my_sync/sync.cuh
@@ -36,50 +36,52 @@ __global__ void sync_test_kernel() {
   }
 }
 
-class Sync {
-    public:
-      int rank_;
-      int world_size_;
+namespace synctest {
+  class Sync {
+      public:
+        int rank_;
+        int world_size_;
 
-      // below are device pointers
-      RankSignals rank_signals_;
-      BarrierState *barrier_state_;
+        // below are device pointers
+        RankSignals rank_signals_;
+        BarrierState *barrier_state_;
 
-      std::vector<void *> ipc_handles_;
+        std::vector<void *> ipc_handles_;
 
-      Sync(BarrierState *barrier_state, const cudaIpcMemHandle_t *handles,
-           const std::vector<int64_t> &offsets, int rank)
-           : rank_(rank),
-             world_size_(offsets.size()),
-             barrier_state_(barrier_state) {
-        for (int i = 0; i < world_size_; i++) {
-          BarrierState *rank_barrier_state;
-          if (i != rank_) {
-            char *handle;
-            CUDACHECK(cudaIpcOpenMemHandle(
-                (void **)&handle, *((const cudaIpcMemHandle_t *)handles[i].data()),
-                cudaIpcMemLazyEnablePeerAccess));
-            ipc_handles_.push_back(handle);
-            handle += offsets[i];
-            rank_barrier_state = (BarrierState *)handle;
-          } else {
-            rank_barrier_state = barrier_state_;
+        Sync(BarrierState *barrier_state, const cudaIpcMemHandle_t *handles,
+             const std::vector<int64_t> &offsets, int rank)
+             : rank_(rank),
+               world_size_(offsets.size()),
+               barrier_state_(barrier_state) {
+          for (int i = 0; i < world_size_; i++) {
+            BarrierState *rank_barrier_state;
+            if (i != rank_) {
+              char *handle;
+              CUDACHECK(cudaIpcOpenMemHandle(
+                  (void **)&handle, *((const cudaIpcMemHandle_t *)handles[i].data()),
+                  cudaIpcMemLazyEnablePeerAccess));
+              ipc_handles_.push_back(handle);
+              handle += offsets[i];
+              rank_barrier_state = (BarrierState *)handle;
+            } else {
+              rank_barrier_state = barrier_state_;
+            }
+            // This is pure pointer math (no access to on-device memory)
+            sg_.signals[i] = &rank_barrier_state->sg;
           }
-          // This is pure pointer math (no access to on-device memory)
-          sg_.signals[i] = &rank_barrier_state->sg;
         }
-      }
 
-      void sync_test(int blocks, int threads) {
-        if (threads % 32 != 0) {
-          throw std::runtime_error("Threads must be a multiple of 32");
-        }
-        sync_test_kernel<<<blocks, threads>>>();
-      }
-
-      ~Sync() {
-          for (auto ptr : ipc_handles_) {
-            CUDACHECK(cudaIpcCloseMemHandle(ptr));
+        void sync_test(int blocks, int threads) {
+          if (threads % 32 != 0 || threads <= 32) {
+            throw std::runtime_error("Threads must be a multiple of 32 greater than 32");
           }
-      }
+          sync_test_kernel<<<blocks, threads>>>();
+        }
+
+        ~Sync() {
+            for (auto ptr : ipc_handles_) {
+              CUDACHECK(cudaIpcCloseMemHandle(ptr));
+            }
+        }
+  }
 }
---
+++ allreduce/csrc/my_sync/sync_test.cu
@@ -0,0 +1,60 @@
+#include "sync.cuh"
+
+#define CUDACHECK(cmd)                                              \
+  do {                                                              \
+    cudaError_t e = cmd;                                            \
+    if (e != cudaSuccess) {                                         \
+      printf("Failed: Cuda error %s:%d '%s'\n", __FILE__, __LINE__, \
+             cudaGetErrorString(e));                                \
+      exit(EXIT_FAILURE);                                           \
+    }                                                               \
+  } while (0)
+
+#define MPICHECK(cmd)                                                  \
+  do {                                                                 \
+    int e = cmd;                                                       \
+    if (e != MPI_SUCCESS) {                                            \
+      printf("Failed: MPI error %s:%d '%d'\n", __FILE__, __LINE__, e); \
+      exit(EXIT_FAILURE);                                              \
+    }                                                                  \
+  } while (0)
+
+int main(int argc, char** argv) {
+  MPI_Init(NULL, NULL);
+
+  int world_size;
+  MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+  int world_rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+  CUDACHECK(cudaSetDevice(world_rank));
+
+  mysync::BarrierState *state;
+  CUDACHECK(cudaMalloc(&state, sizeof(mysync::BarrierState)));
+
+  cudaIpcMemHandle_t cur_rank_handle;
+  cudaIpcMemHandle_t rank_handles[8];
+
+  CUDACHECK(cudaIpcGetMemHandle(&cur_rank_handle, state));
+  MPICHECK(MPI_Allgather(
+    &cur_rank_handle, // void* send_data,
+    sizeof(cudaIpcMemHandle_t),  // int send_count,
+    MPI_BYTE, // MPI_Datatype send_datatype,
+    rank_handles, // void* recv_data,
+    sizeof(cudaIpcMemHandle_t),  // int recv_count,
+    MPI_BYTE, // MPI_Datatype recv_datatype,
+    MPI_COMM_WORLD // MPI_Comm communicator
+  ));
+
+  // Offsets are only necessary for Pytorch bindings
+  // (where tensors are not allocated at the start of a cudaIpcMemHandle)
+  // (that's why we set them to 0 here)
+  std::vector<int64_t> offsets(world_size, 0);
+  mysync::Sync sync(state, rank_handles, offsets, world_rank);
+
+  sync.sync_test(8, 32);
+
+  MPI_Finalize();
+  return EXIT_SUCCESS;
+}
